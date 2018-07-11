b88d6c03a5ea19e6505ee3fc7d7dbca36a7eadbd
JY <jingyin.tang@weather.com>
2018-07-11 15:29:59 -0400

    Work on grb2 sdss plotting

--- mexgdal/test_gdal.m
+++ mexgdal/test_gdal.m
@@ -1,9 +1,31 @@
-%% Simple test
-f = 'data/20030918-093102.img';
-m = gdaldump(f);
-[x, y, z] = readgdalsimple (f);
+% Simple test
+f = 'C:/Users/Administrator/Downloads/grid.global_hqnowrad.20180704T205730Z.NIL.P0D.TILE1@1km.REFDBZ.SFC.grb2';
+% m = gdaldump(f);
+input_option.grid = 1;
+input_option.band = 1;
+% [x, y, zz] = readgdalband (f, input_option);
+% z = z(30:50, 270:290);
+z0 = zz(5500:6000, 4100:4600);
+z = z0;
+z(z <= 10) = nan;
+fprintf('Backgroud\n');
+% set(gca, 'YDir', 'Normal');
 figure;
-image(x, y, z);
-set(gca, 'YDir', 'Normal')
+
+b = imagesc(z);
+set(b,'AlphaData',~isnan(z));
+% colormap('winter');
+hold all;
+
+% overlay a contour
+fprintf('Contouring\n');
+q = z0;
+q(q < 0) = 0;
+a = contour(q, ...
+    'LevelList', (10:5:65), ...
+    'Color', 'k', ...
+    'ShowText', 'on' ...
+);
 
 
+daspect([1 1 1]);
